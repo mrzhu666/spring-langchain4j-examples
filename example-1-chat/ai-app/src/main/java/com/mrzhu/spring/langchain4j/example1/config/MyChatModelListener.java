b2663debca94b49c2d594b4d8358473ef2d18fd0
mrzhu666 <1052712811@qq.com>
2025-10-06 20:43:29 +0800

    fix: add @Component to MyChatModelListener

--- example-1-chat/ai-app/src/main/java/com/mrzhu/spring/langchain4j/example1/config/MyChatModelListener.java
+++ example-1-chat/ai-app/src/main/java/com/mrzhu/spring/langchain4j/example1/config/MyChatModelListener.java
@@ -6,10 +6,12 @@ import dev.langchain4j.model.chat.listener.ChatModelRequestContext;
 import dev.langchain4j.model.chat.listener.ChatModelResponseContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 /**
  * 监听大模型的请求和响应，并打印日志
  */
+@Component
 public class MyChatModelListener implements ChatModelListener {
     private static final Logger log = LoggerFactory.getLogger(MyChatModelListener.class);
     
